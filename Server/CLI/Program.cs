294221ff5b630eecf686cd7fbd1ef205fd40a2f4
Flems At School <354019@via.dk>
2025-09-24 20:34:49 +0200

    FileRepo Dir

--- Server/CLI/Program.cs
+++ Server/CLI/Program.cs
@@ -8,6 +8,7 @@ IUserRepository userRepository = new UserInMemoryRepository() ;
 IPostRepository postRepository = new PostInMemoryRepository() ;
 ICommentRepository commentRepository = new CommentInMemoryRepository() ;
 
+// Repositories Passed to CLI_App
 CLI_App cli_App = new CLI_App(userRepository, commentRepository, postRepository) ;
 
-await cli_App.StartAsync() ;
+await cli_App.StartAsync() ; // Calls CLI_App.cs Which Contains Main Menu
