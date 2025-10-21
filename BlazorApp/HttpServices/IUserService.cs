1b3d4e182187737c6a5e0c161e70cf9515177cda
Flems At School <354019@via.dk>
2025-10-21 20:54:47 +0200

    Use My Razor And Put On My Blazor (Whatch Out Ladies !)

---
+++ BlazorApp/HttpServices/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using ApiContracts_DTO;
+
+namespace BlazorApp.HttpServices;
+
+public interface IUserService
+{
+    public Task<UserDTO> AddUserAsync(CreateUserDTO request);
+    public Task UpdateUserAsync(int id, UpdateUsernameDTO request);
+    // ... more methods
+
+}
